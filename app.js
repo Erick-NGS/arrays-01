0bbe76d90ef841cc76a8b74ef77d1987eb1ba824
Erick Silva <erick.silva@firstdecision.com.br>
2021-03-12 19:18:11 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+const listItem = document.querySelectorAll('li');
+console.log(listItem);
+
+const arrayListItems = Array.from(listItem);
+console.log(arrayListItems);
