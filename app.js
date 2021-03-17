60adbb62b11f00186c31bdcd33c7104fe77fd441
Erick Silva <erick.silva@firstdecision.com.br>
2021-03-17 19:11:34 -0300

    concat

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const numbers = [1, 2, 3];
-console.log(numbers);
+// const numbers = [1, 2, 3];
+// console.log(numbers);
 
-const listItem = document.querySelectorAll('li');
-console.log(listItem);
+// const listItem = document.querySelectorAll('li');
+// console.log(listItem);
 
-const arrayListItems = Array.from(listItem);
-console.log(arrayListItems);
+// const arrayListItems = Array.from(listItem);
+// console.log(arrayListItems);
+
+const testResults = [14, 1.4, 3.14, 99, -1, -10];
+const scoredResults = testResults.concat([33, 2.95]);
+
+console.log(testResults, scoredResults);
