aeab28102f38b5e2f7dffeafc9dce1d6f71f0bd6
Erick Silva <erick.silva@firstdecision.com.br>
2021-03-31 20:25:08 -0300

    video 193

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ const testResults = [14, 1.4, 3.14, 99, -1, -10];
 const scoredResults = testResults.concat([33, 2.95]);
 
 console.log(testResults, scoredResults);
+
+console.log(testResults.indexOf(3.14));
+
+const objArray = [{ name: 'Abe' }, { name: 'Bel' }];
+
+const name = objArray.find((person, index, arrayRes) => {
+  return person.name === 'Bel';
+});
+
+console.log(name);
+
+const nameIndex = objArray.findIndex((person, index, arrayRes) => {
+  return person.name === 'Abe';
+});
+
+console.log(nameIndex);
