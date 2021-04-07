e03498630313e0bc817c91ab9f993d203efe8f79
Erick Silva <erick.silva@firstdecision.com.br>
2021-04-07 10:03:35 -0300

    for each and for of

--- app.js
+++ app.js
@@ -7,23 +7,40 @@
 // const arrayListItems = Array.from(listItem);
 // console.log(arrayListItems);
 
-const testResults = [14, 1.4, 3.14, 99, -1, -10];
-const scoredResults = testResults.concat([33, 2.95]);
+// const testResults = [14, 1.4, 3.14, 99, -1, -10];
+// const scoredResults = testResults.concat([33, 2.95]);
 
-console.log(testResults, scoredResults);
+// console.log(testResults, scoredResults);
 
-console.log(testResults.indexOf(3.14));
+// console.log(testResults.indexOf(3.14));
 
-const objArray = [{ name: 'Abe' }, { name: 'Bel' }];
+// const objArray = [{ name: 'Abe' }, { name: 'Bel' }];
 
-const name = objArray.find((person, index, arrayRes) => {
-  return person.name === 'Bel';
+// const name = objArray.find((person, index, arrayRes) => {
+//   return person.name === 'Bel';
+// });
+
+// console.log(name);
+
+// const nameIndex = objArray.findIndex((person, index, arrayRes) => {
+//   return person.name === 'Abe';
+// });
+
+// console.log(nameIndex);
+
+const prices = [10.99, 5.99, 3.95, 6.69];
+
+const tax = 0.19;
+
+const taxPrices = [];
+
+// for (const price of prices) {
+//   taxPrices.push(price * (1 + tax));
+// }
+
+prices.forEach((price, idx, prices) => {
+  const priceObj = { index: idx, taxPrice: price * (1 + tax) };
+  taxPrices.push(priceObj);
 });
 
-console.log(name);
-
-const nameIndex = objArray.findIndex((person, index, arrayRes) => {
-  return person.name === 'Abe';
-});
-
-console.log(nameIndex);
+console.log(taxPrices);
