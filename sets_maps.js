f0c8256396a6d9505c91e3d7e223c0f144cc45ef
Erick Silva <erick.silva@firstdecision.com.br>
2021-04-26 11:29:40 -0300

    maps

--- sets_maps.js
+++ sets_maps.js
@@ -9,3 +9,25 @@ for (const entry of ids.entries()) {
 }
 
 console.log(ids);
+
+const person1 = { name: 'Erick' };
+const person2 = { name: 'Will' };
+
+const personData = new Map([[person1, [{ date: 'April 26', price: 10 }]]]);
+
+personData.set(person2, [{ date: 'April 23', price: 20 }]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+for (const [key, value] of personData.entries()) {
+  console.log(key, value);
+}
+
+for (const key of personData.keys()) {
+  console.log(key);
+}
+
+for (const value of personData.values()) {
+  console.log(value);
+}
